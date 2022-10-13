b856ba1fd75db247402ae190bc3f6ff0b6b8d66b
Hattington <lehatty@outlook.com>
2022-10-13 23:20:39 +0200

    Ran out of time trying to think of a method to test a rotated shape within bounds and stuff

--- TetrisTemplate/TetrisBlock.cs
+++ TetrisTemplate/TetrisBlock.cs
@@ -10,41 +10,44 @@ using System.Text;
 
 public class TetrisBlock
 {
-    Texture2D emptyCell;
-
     /// <summary>
-    /// Shape of the current Tetris block
+    /// Shape of the current Tetris block as a 2D bool array
     /// </summary>
     public bool[,] Shape { get { return shape; } }
     /// <summary>
-    /// Current location on the grid
+    /// Current location on the grid as a Point
     /// </summary>
     public Point Location { get { return location; } }
 
     /// <summary>
-    /// Color of current Tetris Block
+    /// Returns the color of the Tetris Block
     /// </summary>
     public Color Color { get { return color; } }
-
-    protected Random random = new Random();
+    
+    
+    protected float baseFallSpeed;
+    protected int rotation = 0;
+    protected bool[,] shape;
+    protected bool[] canMoveTo = new bool[4];
     protected Color color;
     protected Color[] possibleColors = {Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Purple };
-    protected bool[,] shape;
-    protected bool[,] detectionPattern;
     protected Point location;
     protected TetrisGrid targetGrid;
-    private bool[] canMoveTo = new bool[4];
+    protected Random random = new Random();
+    protected Texture2D emptyCell;
 
 
-    public TetrisBlock(TetrisGrid targetGrid, Point location)
+
+    public TetrisBlock(TetrisGrid targetGrid, Point location, float baseFallSpeed)
     {
         this.targetGrid = targetGrid;
         this.location = location;
+        this.baseFallSpeed = baseFallSpeed;
         color = possibleColors[random.Next(possibleColors.Length - 1)];
         emptyCell = TetrisGame.ContentManager.Load<Texture2D>("block");
     }
 
-    public void Update()
+    public void Update(GameTime gameTime)
     {
 
     }
@@ -53,131 +56,172 @@ public class TetrisBlock
     {
         if (inputHelper.KeyPressed(Keys.A))
         {
-            Test();
+            MoveTest(shape);
             if (canMoveTo[3]) location.X -= 1;
         }
         if (inputHelper.KeyPressed(Keys.D))
         {
-            Test();
+            MoveTest(shape);
             if (canMoveTo[1]) location.X += 1;
         }
 
         if (inputHelper.KeyPressed(Keys.S))
         {
-            Test();
+            MoveTest(shape);
             if (canMoveTo[2]) location.Y += 1;
         }
 
-        
+        // for debugging
         if (inputHelper.KeyPressed(Keys.W))
         {
-            Test();
+            MoveTest(shape);
             if (canMoveTo[0]) location.Y -= 1;
         }
-
+        
         if (inputHelper.KeyPressed(Keys.Space))
         {
             
         }
 
+        if (inputHelper.KeyPressed(Keys.Right))
+        {
+            RotateCW();
+        }
+
+        if (inputHelper.KeyPressed(Keys.Left))
+        {
+            RotateCCW();
+        }
 
     }
 
     /// <summary>
-    /// Returns a bool list of length 4: 0 = Above, 1 = Right, 2 = Below, 3 = Left
+    /// Returns a bool array of length 4 defined as such: 
+    /// [0] = Up, [1] = Right, [2] = Down, [3] = Left
     /// </summary>
-    public void Test()
+    protected void MoveTest(bool[,] testshape)
     {
-        bool blockIsPresent = false;
-        bool[,] matrix = targetGrid.gridMatrix;
+        bool[,] gridMatrix = targetGrid.gridMatrix;
         canMoveTo = Enumerable.Repeat<bool>(true, 4).ToArray(); // sets all values to true
 
-        for (int y = 0; y < location.Y + 4; y++)
+        for (int y = location.Y; y < location.Y + 4; y++)
         {
-
-            for (int x = 0 ; x < location.X + 4; x++)
+            if (y < 0) continue;
+            for (int x = location.X ; x < location.X + 4; x++) // iterates through possible locations
             {
-                //checks if a part of the shape is in the given coordinates
+                //checks if the 4x4 block is in the given coordinates
                 if (y - location.Y >= 0 && y - location.Y < 4 && x - location.X >= 0 && x - location.X < 4) 
                 {
-                    if (shape[y - location.Y, x - location.X]) blockIsPresent = true;
-                    else
+                    // This part has to be nested else you get problems with indexes being out of range
+                    // It checks if a part of the shape is present within the given coordinates
+                    if (testshape[y - location.Y, x - location.X]) 
                     {
-                        blockIsPresent = false;
-                        continue;
-                    }
-                }
+                        // this part tests for "edge" cases (see what i did there?)
+                        if (y == targetGrid.Height - 1) canMoveTo[2] = false;
+                        if (x == 0)                     canMoveTo[3] = false;
+                        if (x == targetGrid.Width - 1)  canMoveTo[1] = false;
 
-                if (blockIsPresent) // checks around the shape
-                {
-                    // this part tests for "edge" cases (see what i did there?)
-                    if (y == targetGrid.Height - 1) canMoveTo[2] = false;
-                    if (x == 0)                     canMoveTo[3] = false;
-                    if (x == targetGrid.Width - 1)  canMoveTo[1] = false;
+                        // if the shape is not near an edge, this kicks in
+                        if (y > 0 && y < targetGrid.Height - 1)
+                        {
+                            if (gridMatrix[y - 1, x])   canMoveTo[2] = false;
+                            if (gridMatrix[y + 1, x])   canMoveTo[0] = false;
 
-                    // if the shape is not near an edge, this kicks in
-                    if (y > 0 && y < targetGrid.Height - 1)
-                    {
-                        if (matrix[y - 1, x]) canMoveTo[2] = false;
-                        if (matrix[y + 1, x]) canMoveTo[0] = false;
-
-                    }
-                    if (x > 0 && x < targetGrid.Width - 1)
-                    {
-                        if (matrix[y, x - 1]) canMoveTo[3] = false;
-                        if (matrix[y, x + 1]) canMoveTo[1] = false;
+                        }
+                        if (x > 0 && x < targetGrid.Width - 1)
+                        {
+                            if (gridMatrix[y, x - 1])   canMoveTo[3] = false;
+                            if (gridMatrix[y, x + 1])   canMoveTo[1] = false;
+                        }
                     }
                 }
             }
         }
-        
-        // check which side got triggered and update result
     }
+
+    protected bool RotateTest(bool[,] rotatedShape, int offsetX, int offsetY) // doesn't fuckin work
+    {
+        bool[,] gridMatrix = targetGrid.gridMatrix;
+        Point offsetLocation = new Point(offsetY + location.Y, offsetX + location.X);
+        for (int y = offsetLocation.Y; y < offsetLocation.Y + 4; y++)
+        {
+            for (int x = offsetLocation.X; x < offsetLocation.X + 4; x++) // iterates through possible offsetLocations
+            {
+                if (x < 0)
+                {
+                    if (rotatedShape[0, -1 + (offsetLocation.X + x) * -1] || 
+                        rotatedShape[1, -1 + (offsetLocation.X + x) * -1] || 
+                        rotatedShape[2, -1 + (offsetLocation.X + x) * -1] || 
+                        rotatedShape[3, -1 + (offsetLocation.X + x) * -1]) 
+                        
+                        return false;
+                }
+                if (x > targetGrid.Width - 1)
+                {
+                    if (rotatedShape[0, 4 - (x - targetGrid.Width - 1)] ||
+                        rotatedShape[1, 4 - (x - targetGrid.Width - 1)] ||
+                        rotatedShape[2, 4 - (x - targetGrid.Width - 1)] ||
+                        rotatedShape[3, 4 - (x - targetGrid.Width - 1)]) 
+                        
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
     /// <summary>
-    /// Rotates shape ClockWise
+    /// Rotates shape ClockWise, Test() built in, no need to call it seperately
     /// </summary>
     public void RotateCW()
     {
-        bool[,] a = shape;
-        bool[,] rotatedA = new bool[4, 4] {
+        bool[,] a = shape; 
+        bool[,] rotated = new bool[4, 4] {
             {a[3, 0], a[2, 0], a[1, 0], a[0, 0] },
             {a[3, 1], a[2, 1], a[1, 1], a[0, 1] },
             {a[3, 2], a[2, 2], a[1, 2], a[0, 2] },
             {a[3, 3], a[2, 3], a[1, 3], a[0, 3] }};
-
-
+        for (int x = -1; x < 2; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+                if (RotateTest(rotated, x, y)) shape = rotated;
+            }
+        }
     }
 
     /// <summary>
-    /// Rotates shape Counter ClockWise
+    /// Rotates shape Counter ClockWise, Test() built in, no need to call it seperately
     /// </summary>
     public void RotateCCW()
     {
+        bool[,] a = shape;
+        bool[,] rotated = new bool[4, 4] {
+        {a[0, 3], a[1, 3], a[2, 3], a[3, 3] },
+        {a[0, 2], a[1, 2], a[2, 2], a[3, 2] },
+        {a[0, 1], a[1, 1], a[2, 1], a[3, 1] },
+        {a[0, 0], a[1, 0], a[2, 0], a[3, 0] }};
 
-        
-            bool[,] a = shape;
-            bool[,] rotated = new bool[4, 4] {
-            {a[0, 3], a[1, 3], a[2, 3], a[3, 3] },
-            {a[0, 2], a[1, 2], a[2, 2], a[3, 2] },
-            {a[0, 1], a[1, 1], a[2, 1], a[3, 1] },
-            {a[0, 0], a[1, 0], a[2, 0], a[3, 0] }};
-            this.shape = rotated;
-        
+        for (int x = -1; x < 2; x++)
+        {
+            for(int y = 0; y < 2; y++)
+            {
+                if (RotateTest(rotated, x, y)) shape = rotated;
+            }
+        }
     }
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         Vector2 gridPosition = targetGrid.Position;
-        bool[,] gridMatrix = targetGrid.gridMatrix;
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                if (shape[i, j])
+                if (shape[i, j] && location.Y + i >= 0)
                 {
                     spriteBatch.Draw(emptyCell, new Vector2(gridPosition.X + ((j + location.X) * emptyCell.Width), gridPosition.Y + ((i + location.Y) * emptyCell.Height)), color);
                 }
-
             }
         }
     }
@@ -187,8 +231,8 @@ public class TetrisBlock
 /// </summary>
 class T : TetrisBlock
 {
-    public T(TetrisGrid targetGrid, Point location)
-        : base( targetGrid, location)
+    public T(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base( targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
@@ -205,8 +249,8 @@ class T : TetrisBlock
 /// </summary>
 class L1 : TetrisBlock
 {
-    public L1(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public L1(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
@@ -223,8 +267,8 @@ class L1 : TetrisBlock
 /// </summary>
 class L2 : TetrisBlock
 {
-    public L2(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public L2(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
@@ -241,8 +285,8 @@ class L2 : TetrisBlock
 /// </summary>
 class Long : TetrisBlock
 {
-    public Long(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public Long(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, true , false, false},
@@ -259,8 +303,8 @@ class Long : TetrisBlock
 /// </summary>
 class SQ : TetrisBlock
 {
-    public SQ(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public SQ(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
@@ -278,8 +322,8 @@ class SQ : TetrisBlock
 /// </summary>
 class D1 : TetrisBlock
 {
-    public D1(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public D1(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
@@ -296,8 +340,8 @@ class D1 : TetrisBlock
 /// </summary>
 class D2 : TetrisBlock
 {
-    public D2(TetrisGrid targetGrid, Point location)
-        : base(targetGrid, location)
+    public D2(TetrisGrid targetGrid, Point location, float baseFallSpeed)
+        : base(targetGrid, location, baseFallSpeed)
     {
         base.shape = new bool[4, 4]{
                 {false, false, false, false},
